13f12a3bcdec101ef5ff6d26d75a2bdbf9bbfff6
heyhiru <hirentimbadiya74@gmail.com>
2022-11-25 23:20:34 +0530

    improved commenting

--- script.js
+++ script.js
@@ -69,7 +69,6 @@ function onClickRPS(playerChoice) {
 function playGame() {
     // use querySelector to select all RPS Buttons
     let RPSButtons = document.querySelectorAll('.rpsButton');
-
     // * Adds an on click event listener to each RPS button and every time you click it, it calls the onClickRPS function with the RPS button that was last clicked *
     // 1. loop through the buttons using a forEach loop
     // 2. Add a 'click' event listener to each button
@@ -79,7 +78,7 @@ function playGame() {
         rpsButton.onclick = () => onClickRPS(rpsButton.value);
     });
 
-    // Add a click listener to the end game button that runs the endGame() function on click
+    // Added a click listener to the end game button that runs the endGame() function on click
     document.getElementById('endGameButton').onclick = () => endGame();
 }
 
